c04c9af57f345e0fb94e263dacd698c689b35ed9
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-08-11 10:06:37 -0500

    Add name to Trader class and create player(as Trader object)

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
 Director = require "director"
+Trader = require "trader"
 Trade = require "scenes.trade"
 
 function love.load()
   width, height = love.graphics.getDimensions()
+  player = Trader:new("Player")
   Director:changeScene(Trade, "Placeholder")
 end
 
--- trader.lua
+++ trader.lua
@@ -1,7 +1,7 @@
 local Trader = {}
 
-function Trader:new()
-  local o = {credits = 1000, q={}, price={}}
+function Trader:new(name)
+  local o = {name = name, credits = 1000, q={}, price={}}
   setmetatable(o, self)
   self.__index = self
   return o
@@ -9,9 +9,9 @@ end
 
 function Trader:sell(good, q, price, trader)
   assert(q <= self.q[good],
-    "Tried to sell "..q.." "..good.." when trader only had "..self.q[good])
+    "Tried to sell "..q.." "..good.." when "..self.name.." only had "..self.q[good])
   assert(trader.credits >= price * q,
-    "Buyer had insufficient credits: "..trader.credits.."/"..(price * q))
+    trader.name.." had insufficient credits: "..trader.credits.."/"..(price * q))
   self.q[good] = self.q[good] - q
   if not trader.q[good] then
     trader.q[good] = 0
