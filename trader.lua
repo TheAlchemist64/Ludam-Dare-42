3c6913e82869a396d12f8fa5bd69e21b212e7cfa
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-08-11 09:27:36 -0500

    Change sell function to take price as argument

--- trader.lua
+++ trader.lua
@@ -7,32 +7,18 @@ function Trader:new()
   return o
 end
 
-function Trader:sell(good, q, trader)
+function Trader:sell(good, q, price, trader)
   assert(q <= self.q[good],
     "Tried to sell "..q.." "..good.." when trader only had "..self.q[good])
-  assert(trader.credits >= self.price[good],
-    "Buyer had insufficient credits: "..trader.credits.."/"..self.price[good])
+  assert(trader.credits >= price * q,
+    "Buyer had insufficient credits: "..trader.credits.."/"..(price * q))
   self.q[good] = self.q[good] - q
   if not trader.q[good] then
     trader.q[good] = 0
   end
   trader.q[good] = trader.q[good] + q
-  self.credits = self.credits + self.price[good] * q
-  trader.credits = trader.credits - self.price[good] * q
-end
-
-function Trader:buy(good, q, trader)
-  assert(q <= trader.q[good],
-    "Tried to buy "..q.." "..good.." when trader only had "..self.q[good])
-  assert(self.credits >= trader.price[good],
-    "Buyer had insufficient credits: "..self.credits.."/"..trader.price[good])
-  trader.q[good] = trader.q[good] - q
-  if not self.q[good] then
-    self.q[good] = 0
-  end
-  self.q[good] = self.q[good] + q
-  trader.credits = trader.credits + trader.price[good] * q
-  self.credits = self.credits - trader.price[good] * q
+  self.credits = self.credits + price * q
+  trader.credits = trader.credits - price * q
 end
 
 return Trader
