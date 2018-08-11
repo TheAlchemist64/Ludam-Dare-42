516ddcefda04c645b152e484885edc5b6a125ea7
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-08-11 10:28:39 -0500

    Show Player and Trader credit balance on trade screen

--- scenes/trade.lua
+++ scenes/trade.lua
@@ -10,6 +10,7 @@ function Trade:load (loc, trader)
   self.h1 = love.graphics.newFont(h1)
   self.h2 = love.graphics.newFont(h2)
   self.h3 = love.graphics.newFont(h3)
+  self.default = love.graphics.newFont()
 end
 
 function Trade:update (dt)
@@ -21,10 +22,15 @@ function Trade:draw ()
   love.graphics.setFont(self.h1)
   love.graphics.print(self.loc, width / 2 - self.loc:len() * h1/4, 0)
   love.graphics.setFont(self.h2)
-  love.graphics.rectangle("line", 0, h1 + 3, width/2, h2)
+  love.graphics.rectangle("line", 0, h1 + 3, width/2, h2 + 12)
   love.graphics.print(player.name, width/4 - player.name:len() * h2/4, h1)
-  love.graphics.rectangle("line", width/2, h1 + 3, width/2, h2)
+  love.graphics.rectangle("line", width/2, h1 + 3, width/2, h2 + 12)
   love.graphics.print(self.trader.name, width * 0.75 - self.trader.name:len() * h2/4, h1)
+  love.graphics.setFont(self.default)
+  local pc = player.credits.." credits"
+  love.graphics.print(pc, width/4 - pc:len() * 3, h1 + h2)
+  local tc = self.trader.credits.." credits"
+  love.graphics.print(tc, width * 0.75 - tc:len() * 3, h1 + h2)
 end
 
 return Trade
