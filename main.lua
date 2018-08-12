2ca95a46400cfc49af9c5174a240964b6ffc4454
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-08-12 15:01:10 -0500

    Create table of trade goods

--- main.lua
+++ main.lua
@@ -14,6 +14,26 @@ function love.load()
   width, height = love.graphics.getDimensions()
   rng = love.math.newRandomGenerator()
   rng:setSeed(os.time())
+  --name=min price
+  goods = {
+    Fuel=1,
+    Food=1,
+    Ore=2,
+    Alloys=4,
+    Fabric=2,
+    Clothing=4,
+    Machinery=3,
+    Computers=5,
+    Gold=6,
+    Platinum=7,
+    Gems=6,
+    Perfume=5,
+    Powder=7,
+    Phasers=7,
+    Drones=8,
+    Artifacts=10
+  }
+  contraband = {'powder'}
   player = Trader:new("Player")
   player.q['fuel'] = 10
   player['loc'] = 'Star 1'
