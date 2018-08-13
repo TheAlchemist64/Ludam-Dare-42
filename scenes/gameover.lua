c249cec1d9c869bdb7b5a95ec639b64236d79286
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-08-12 23:44:22 -0500

    Fixgame over screen text position

--- scenes/gameover.lua
+++ scenes/gameover.lua
@@ -7,7 +7,7 @@ function GameOver:update (dt) end
 function GameOver:draw ()
   love.graphics.setFont(self.font)
   local text = "Game Over"
-  love.graphics.print(text, width/2 - text:len() * 16, height/2 - 16)
+  love.graphics.print(text, width/2 - text:len() * 16 + 64, height/2 - 32)
 end
 
 return GameOver
