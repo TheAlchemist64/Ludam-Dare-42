e865e62b10d35ef19c49123d08c6e2d9b4a0939c
Daniel Sheppard <TheAlchemist64@users.noreply.github.com>
2018-08-13 15:13:21 -0500

    Remove unused function

--- trader.lua
+++ trader.lua
@@ -7,18 +7,4 @@ function Trader:new(name)
   return o
 end
 
-function Trader:sell(good, q, price, trader)
-  assert(q <= self.q[good],
-    "Tried to sell "..q.." "..good.." when "..self.name.." only had "..self.q[good])
-  assert(trader.credits >= price * q,
-    trader.name.." had insufficient credits: "..trader.credits.."/"..(price * q))
-  self.q[good] = self.q[good] - q
-  if not trader.q[good] then
-    trader.q[good] = 0
-  end
-  trader.q[good] = trader.q[good] + q
-  self.credits = self.credits + price * q
-  trader.credits = trader.credits - price * q
-end
-
 return Trader
